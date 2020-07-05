d9be40f158b6845f3351a1fb4886ced0fdebb109
fisnikshatri <fs39047@ubt-uni.net>
2020-07-05 02:58:40 +0200

    ndryshime

--- projekti/register/roles/paneli/add.php
+++ projekti/register/roles/paneli/add.php
@@ -8,14 +8,14 @@ include 'connection.php';
   $userType = $_POST['userType'];
   
 if(!$_POST['submit']){
-	// you can remove this echo code and add alert using JS or use required tag in your input feilds.
+	
 	
   echo "Te gjitha fushat duhet te plotesohen";
   
 }
 
 else {
- // insert into tableName (feilds) values (variables)
+
  
 $sql = "INSERT INTO roles (username,password,user_type)
 VALUES ('$username', '$password', '$userType')";
@@ -30,17 +30,17 @@ if (mysqli_query($conn, $sql)) {
 
 <html>
 <head>
-<title>Add Data</title>
+<title>Krijimi i Userit</title>
 </head>
 
 <body>
    
-	<h2>Add Updates from this menu</h2>
+	<h2>Shto Userin duke plotesuar rreshtat me poshte!</h2>
 		<form action="add.php" method="POST">
 			Name: <input type="text" name="username" placeholder="Username" value="" required><br><br>
 			Password: <input type="password" name="password" placeholder="Password" value="" required><br><br>
-			<!--Lloji: <textarea rows="4" cols="50" name="text" value="" required></textarea><br>-->
-			<label for="userType">Shto si :</label>
+				<label for="userType">Shto si :</label>
+
                 <input type="radio" name="userType" value="qytetar" class="costum-radio" 
                         required>&nbsp;Qytetar |
                 <input type="radio" name="userType" value="administrator" class="costum-radio" 
@@ -50,13 +50,4 @@ if (mysqli_query($conn, $sql)) {
 	<br>
 			<input type="submit" name="submit" value="add"/></center>
 </body>
-
-<!--
-	Similarly you can make delete and updates pages with little changes in sql query and here and there. If you need to learn those too
-	please check my youtube channel NOSGENE as i am running a full stack web development course there right now.
- -->
-
- <!--
-      ENCODED BY RAMEEZ SAFDAR / For more web and other programmings check out my channel nosgene https://www.youtube.com/channel/UCYbUaMVWujooISm4m7NDIeg
- -->
 </html>
