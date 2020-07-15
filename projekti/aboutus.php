e164611d97cd28309e60a5d42312656d5b64dcd6
fisnikshatri <fs39047@ubt-uni.net>
2020-07-15 19:22:14 +0200

    ndryshime

--- projekti/aboutus.php
+++ projekti/aboutus.php
@@ -5,21 +5,31 @@
     <div class="row py-3">
         <div class="col-md-8">
             <div class="card">
-                <img src="../banner4.jpg" class="card-img-top" alt="...">
+                <img src="../projekti/banner4.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Night Club</h5>
                     <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero placeat quia enim quibusdam impedit ad.</p>
-                    <a href="/index.php" class="btn btn-primary">Go somewhere</a>
+                    <a href="../projekti/index.php" class="btn btn-primary">Shko te HomePage</a>
                 </div>
             </div>
         </div>
 
         <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
+            <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Contact NightClub</h5>
+                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, accusamus!</p>
+                    <a href="../projekti/contact/contactus.php" class="btn btn-primary">Shko te Contact US</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Login NightClub</h5>
+                    <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, accusamus!</p>
+                    <a href="../projekti/register/index.php" class="btn btn-primary">Shko te Logini</a>
                 </div>
             </div>
         </div>
